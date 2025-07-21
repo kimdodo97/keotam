b4190446cf125c1566a76e9f9fd418714aa85305
kimdodo97 <eh4536@naver.com>
2025-07-21 22:57:52 +0900

    feat : 카페 존재하지 않음 예외 추가

--- backend/src/main/java/com/keotam/cafe/exception/CafeNotFound.java
+++ backend/src/main/java/com/keotam/cafe/exception/CafeNotFound.java
@@ -1,4 +1,17 @@
 package com.keotam.cafe.exception;
 
-public class CafeNotFound {
+
+import com.keotam.global.exception.KeotamException;
+
+public class CafeNotFound extends KeotamException {
+    private static final String MESSAGE = "해당 카페가 존재하지 않습니다.";
+
+    public CafeNotFound() {
+        super(MESSAGE);
+    }
+
+    @Override
+    public int getStatusCode() {
+        return 404;
+    }
 }
