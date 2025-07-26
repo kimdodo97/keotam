f1a3ef0e9485bddf8cf0687acaedbe196e79a538
kimdodo97 <eh4536@naver.com>
2025-07-27 00:11:37 +0900

    feat : 투표 생성시 어드민 유저의 이름도 추가 요청

--- backend/src/main/java/com/keotam/vote/dto/request/VoteCreateRequest.java
+++ backend/src/main/java/com/keotam/vote/dto/request/VoteCreateRequest.java
@@ -8,11 +8,13 @@ public class VoteCreateRequest {
     private Long cafeId;
     private String password;
     private String voteName;
+    private String adminName;
 
     @Builder
-    public VoteCreateRequest(Long cafeId, String password, String voteName) {
+    public VoteCreateRequest(Long cafeId, String password, String voteName, String adminName) {
         this.cafeId = cafeId;
         this.password = password;
         this.voteName = voteName;
+        this.adminName = adminName;
     }
 }
