e7f0b16523cb4918f063100f63c3c7caa33fc8ed
kimdodo97 <eh4536@naver.com>
2025-07-26 23:55:03 +0900

    feat : 투표와 투표자 연관관계 추가

--- backend/src/main/java/com/keotam/vote/domain/Vote.java
+++ backend/src/main/java/com/keotam/vote/domain/Vote.java
@@ -7,6 +7,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -31,6 +34,9 @@ public class Vote {
     @JoinColumn(name = "cafe_id")
     private Cafe cafe;
 
+    @OneToMany(mappedBy="vote", cascade = CascadeType.ALL)
+    private List<Voter> voters = new ArrayList<>();
+
     @Builder
     public Vote(Long id, String manageUrl, String joinUrl, String voteName, String votePw, VoteStatus status, Cafe cafe) {
         this.id = id;
@@ -40,5 +46,9 @@ public class Vote {
         this.votePw = votePw;
         this.status = status;
         this.cafe = cafe;
+    }
+
+    public void addVoter(Voter voter) {
+        voters.add(voter);
     }
 }
