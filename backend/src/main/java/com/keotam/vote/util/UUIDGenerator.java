327732b8ed5cdf9200924fee2c30540197cf1d96
kimdodo97 <eh4536@naver.com>
2025-07-27 00:11:58 +0900

    fix : UUID 생성시 VoterType 파라미터 받는걸로 변경

--- backend/src/main/java/com/keotam/vote/util/UUIDGenerator.java
+++ backend/src/main/java/com/keotam/vote/util/UUIDGenerator.java
@@ -1,13 +1,13 @@
 package com.keotam.vote.util;
 
-import com.keotam.vote.domain.UUIDType;
+import com.keotam.vote.domain.VoterType;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
 @Component
 public class UUIDGenerator {
-    public String generateUUID(UUIDType type) {
+    public String generateUUID(VoterType type) {
         return UUID.randomUUID().toString();
     }
 }
