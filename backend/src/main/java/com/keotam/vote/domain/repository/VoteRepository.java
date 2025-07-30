297755bc57651a243465c4d67106ab5cf65ff579
kimdodo97 <eh4536@naver.com>
2025-07-30 23:45:51 +0900

    fix : ShareUuid(참여UUID) 로 조회 메서드 추가

--- backend/src/main/java/com/keotam/vote/domain/repository/VoteRepository.java
+++ backend/src/main/java/com/keotam/vote/domain/repository/VoteRepository.java
@@ -4,6 +4,9 @@ import com.keotam.vote.domain.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
+    Optional<Vote> findByShareUuid(String shareUuid);
 }
